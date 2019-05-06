a286fe64c7704aa0b54b0fe5bda707ea06452d90
Hiroto Yoshidome <kichiru.3161026@gmail.com>
2019-05-06 12:29:38 +0900

    名前空間を使用したレンダリングを追加

--- hello/urls.py
+++ hello/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'hello'
+
 urlpatterns = [
     path('', views.hello, name='hello'),
     path('specifics/<int:question_id>/', views.detail, name='detail'),
