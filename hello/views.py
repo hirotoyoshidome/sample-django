5ac0a697f8732e59303021906164924db2fab358
Hiroto Yoshidome <kichiru.3161026@gmail.com>
2019-05-06 12:22:12 +0900

    get_object_or_404メソッドを使用した実装を追加

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 #from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 
 def hello(request):
@@ -13,7 +13,8 @@ def hello(request):
     return render(request, 'hello/hello.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at quetion %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'hello/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
