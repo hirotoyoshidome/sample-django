6f7e9d798d5e586c09eb48704bd38a5a726e6d1e
Hiroto Yoshidome <kichiru.3161026@gmail.com>
2019-05-06 15:43:00 +0900

    一覧表示と詳細表示を汎用ビューを使用するように修正

--- hello/urls.py
+++ hello/urls.py
@@ -5,8 +5,11 @@ from . import views
 app_name = 'hello'
 
 urlpatterns = [
-    path('', views.hello, name='hello'),
-    path('specifics/<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('', views.HelloView.as_view(), name='hello'),
+    # path('', views.hello, name='hello'),
+    path('specifics/<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # path('specifics/<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,26 +1,39 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 #from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
-def hello(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    #template = loader.get_template('hello/hello.html')
-    context = {
-            'latest_question_list': latest_question_list,
-    }
-    #return HttpResponse(template.render(context, request))
-    return render(request, 'hello/hello.html', context)
+#def hello(request):
+#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#    #template = loader.get_template('hello/hello.html')
+#    context = {
+#            'latest_question_list': latest_question_list,
+#    }
+#    #return HttpResponse(template.render(context, request))
+#    return render(request, 'hello/hello.html', context)
+class HelloView(generic.ListView):
+    template_name = 'hello/hello.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'hello/detail.html', {'question': question})
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'hello/results.html', {'question': question})
+#def detail(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, 'hello/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'hello/detail.html'
+
+#def results(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, 'hello/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'hello/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
