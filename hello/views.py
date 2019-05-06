adea7f976d4ad8c42bc847081ba39f2f476fcf17
Hiroto Yoshidome <kichiru.3161026@gmail.com>
2019-05-06 15:12:48 +0900

    formの実装

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 #from django.template import loader
 from django.shortcuts import get_object_or_404, render
-from .models import Question
+from django.urls import reverse
+
+from .models import Choice, Question
 
 def hello(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -17,9 +19,21 @@ def detail(request, question_id):
     return render(request, 'hello/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'hello/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'hello/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice .",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('hello:results', args=(question.id,)))
+
 
