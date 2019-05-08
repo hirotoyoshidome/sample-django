1d5d943a0d7ae6077eb3abc5f2f42d6b0f4860e1
Hiroto Yoshidome <kichiru.3161026@gmail.com>
2019-05-08 21:42:31 +0900

    モデルのテストケース追加

--- hello/tests.py
+++ hello/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import Question
 
-# Create your tests here.
+class QuestionModelTests(TestCase):
+    def test_str_with_question(self):
+        exp = "test"
+        question = Question(question_text=exp)
+        self.assertIs(question.__str__(), exp)
